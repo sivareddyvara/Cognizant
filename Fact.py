e8fd62247a497056db09c9b38fb97e5ef676d44f
sivareddyvara <sivaready999@gmail.com>
2023-12-27 19:02:56 +0530

    add 2 program

--- Fact.py
+++ Fact.py
@@ -6,3 +6,14 @@ def siva(x):
     return count      
 x='SiVaReDdY'
 print(siva(x))
+######################################
+
+x=[1,2,4]  #output=[6,5,3]
+
+s=[]
+for i in range(len(x)):
+    s.append(sum(x[:i])+sum(x[i+1:]))
+print(s)
+
+
+
